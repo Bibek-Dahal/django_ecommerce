d64ffec3b1e16f8ccf6826d8bbf5160431909112
Bibek <bibekdahal479@gmail.com>
2021-10-02 12:46:46 +0545

    seven  commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -168,9 +168,9 @@ ESEWA_SECRET_KEY = os.environ.get('ESEWA_SECRET_KEY',True)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-# STATICFILES_DIRS =[
-# os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS =[
+os.path.join(BASE_DIR, 'static')
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT  = BASE_DIR / 'static'
 
