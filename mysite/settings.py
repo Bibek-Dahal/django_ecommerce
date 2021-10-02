54cbd19b326e2719486a3537d77d0a3b92c79064
Bibek <bibekdahal479@gmail.com>
2021-10-02 12:36:39 +0545

    fifth  commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY',True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD':os.environ['PASSWORD'],
+        'NAME': os.environ.get('NAME',True),
+        'USER': os.environ.get('USER',True),
+        'PASSWORD':os.environ.get('PASSWORD',True),
         'HOST': 'bibekecom.herokuapp.com',
         'PORT': '3306',
     }
