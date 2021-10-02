73d9285e0f6fb4df441e4292f8c95af958d95958
Bibek <bibekdahal479@gmail.com>
2021-10-02 12:28:33 +0545

    fourth commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
 os.path.join(BASE_DIR, 'static')
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT  = BASE_DIR / 'static'
 
 # MEDIA_URL  = '/dk7xo8rng/'
