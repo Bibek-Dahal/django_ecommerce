5318cf2a66106be04af05eb56d84b4bf734c7cc6
Bibek <bibekdahal479@gmail.com>
2021-10-09 21:20:20 +0545

    fixing cart quan 2

--- store/views.py
+++ store/views.py
@@ -20,7 +20,7 @@ def Home(request):
    
     x=ProductInventory.objects.filter(sku = '12888blacknoteredmi')
     
-    return render(request,"store/home.html",{'query_set':query_set})
+    return render(request,"store/home.html",{'query_set':query_set,'session':request.session.get('cart')})
     
 class ProductSearchView(View):
     def get(self,request,*args,**kwargs):
