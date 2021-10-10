5a805cf183b843f98c03e6e506b77059882aba04
Bibek <bibekdahal479@gmail.com>
2021-10-10 21:11:59 +0545

    minified js removed

--- static/js/catagory.js
+++ static/js/catagory.js
@@ -1,1 +1,43 @@
-function getCookie(t){let o=null;if(document.cookie&&""!==document.cookie){const e=document.cookie.split(";");for(let n=0;n<e.length;n++){const c=e[n].trim();if(c.substring(0,t.length+1)===t+"="){o=decodeURIComponent(c.substring(t.length+1));break}}}return o}const csrftoken=getCookie("csrftoken");function addToCart(t,o,e){$.ajax({method:"post",url:"/cart/add-to-cart/",cache:!1,data:{id:t,title:e,price:o,quantity:1,csrfmiddlewaretoken:csrftoken},success:t=>{$("#cart-quan").html(t.length)}})}$(".container").hide(),$(window).on("load",function(){$("#spin").remove(),$(".container").show()});
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  const csrftoken = getCookie("csrftoken");
+  
+  function addToCart(id, price, title) {
+    ////console.log("add_to_cart clicked");
+    $.ajax({
+      method: "post",
+      url: "/cart/add-to-cart/",
+      data: {
+        id: id,
+        title: title,
+        price: price,
+        quantity: 1,
+        csrfmiddlewaretoken: csrftoken,
+      },
+      success: (response) => {
+        ////console.log(response);
+        ////console.log('length',response.length);
+        $('#cart-quan').html(response.length);
+      },
+    });
+  }
+  $(".container").hide();
+  $(window).on("load", function () {
+    ////console.log("window loaded");
+    $("#spin").remove();
+    $(".container").show();
+  
+  });
--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,1 +1,86 @@
-let uuid=$("#u").val();function getCookie(e){let t=null;if(document.cookie&&""!==document.cookie){const o=document.cookie.split(";");for(let n=0;n<o.length;n++){const c=o[n].trim();if(c.substring(0,e.length+1)===e+"="){t=decodeURIComponent(c.substring(e.length+1));break}}}return t}const csrftoken=getCookie("csrftoken");var config={publicKey:"test_public_key_2cb744b91026476980e513bd0c377ef2",productIdentity:"{{uuid}}",productName:"Dragon",productUrl:"http://gameofthrones.wikia.com/wiki/Dragons",paymentPreference:["KHALTI","EBANKING","MOBILE_BANKING","CONNECT_IPS","SCT"],eventHandler:{onSuccess(e){$(".container").addClass("d-none"),$("#spin").attr("class","d-flex justify-content-center"),$.ajax({type:"post",url:"/cart/verify/",cache:!1,data:{token:e.token,amount:e.amount,csrfmiddlewaretoken:csrftoken},success:e=>{"success"==e.msg?$("#payment_form").submit():window.location.href="/cart/transaction-failed"},error:e=>{}})},onError(e){},onClose(){}}},checkout=new KhaltiCheckout(config),btn=document.getElementById("payment-button");btn.onclick=function(){checkout.show({amount:1e3})};
+$('input[type=radio][name=id]').change(function () {
+    $('#su').val(`https://bibekecom.herokuapp.com/cart/v-esewa/?cid=${this.value}`);
+    console.log($('#su').val())
+});
+let uuid = $('#u').val();
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie("csrftoken");
+var config = {
+    publicKey: "test_public_key_2cb744b91026476980e513bd0c377ef2",
+    productIdentity: '{{uuid}}',
+    productName: "Dragon",
+    productUrl: "http://gameofthrones.wikia.com/wiki/Dragons",
+    paymentPreference: [
+        "KHALTI",
+        "EBANKING",
+        "MOBILE_BANKING",
+        "CONNECT_IPS",
+        "SCT",
+    ],
+    eventHandler: {
+
+        onSuccess(payload) {
+            $(".container").addClass("d-none");
+            $("#spin").attr("class", "d-flex justify-content-center");
+            $.ajax({
+                type: 'post',
+                url: '/cart/verify/',
+                data: {
+                    token: payload.token,
+                    amount: payload.amount,
+                    csrfmiddlewaretoken: csrftoken
+                },
+                success: (response) => {
+
+                    if (response.msg == "success") {
+
+                        $('#payment_form').submit();
+                    } else {
+                        window.location.href = '/cart/transaction-failed'
+                    }
+
+                },
+                error: (error) => {
+                    ///.log(error)
+
+                }
+            });
+
+
+        },
+        onError(error) {
+            ///.log(error);
+        },
+        onClose() {
+            ///.log("widget is closing");
+        },
+    },
+};
+
+var checkout = new KhaltiCheckout(config);
+var btn = document.getElementById("payment-button");
+btn.onclick = function () {
+
+    if ("{{request.user}}" === "AnonymousUser") {
+        window.location.href = "{% url 'account:signup'%}";
+    } else {
+        checkout.show({
+            amount: 1000
+        });
+    }
+};
