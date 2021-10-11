37dd5ac287f25fd1b2d1a4b9f05e7ccd140ee6e4
Bibek <bibekdahal479@gmail.com>
2021-10-11 21:57:12 +0545

    create session function object name changed

--- cart/context_processor.py
+++ cart/context_processor.py
@@ -1,7 +1,7 @@
 from .cart import Cart
 
 def create_session(request):
-    cart_session = Cart(request)
-    return({'cart_quantity':cart_session.show_quantity()})
+    cart_quantity = Cart(request)
+    return({'cart_quantity':cart_quantity.show_quantity()})
 
 
